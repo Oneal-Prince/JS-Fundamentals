0ed1cf91ded8fbf6f13eb19cf0a1b91d0b4f0cb2
monia <ornellagrace0412@gmail.com>
2025-07-11 17:37:14 +0200

    Adding new line and deleting line

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,10 +1,10 @@
-// process.argv.slice(2).forEach((arg, index) => 
-  // {
-  // console.log(`Argument ${index + 1}:${arg}`);
-// });
 
 const [, , firstArg] = process.argv;
 if (firstArg) {
   console.log(firstArg || "");
+}
+
+if (process.argv[2] === undefined) {
+    console.log("No argument");
 }
 
