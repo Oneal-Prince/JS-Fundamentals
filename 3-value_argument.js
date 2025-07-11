4567033a6a27b9fbb9526c5e2ae7ffadb0e6f038
monia <ornellagrace0412@gmail.com>
2025-07-11 17:18:36 +0200

    Modify a line

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,10 +1,10 @@
-process.argv.slice(2).forEach((arg, index) => 
-  {
-  console.log(`Argument ${index + 1}:${arg}`);
-});
+// process.argv.slice(2).forEach((arg, index) => 
+  // {
+  // console.log(`Argument ${index + 1}:${arg}`);
+// });
 
 const [, , firstArg] = process.argv;
 if (firstArg) {
-  console.log(firstArg);
+  console.log(firstArg || "");
 }
 
