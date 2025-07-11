8d8fe1f704e886272b497185761579c52613c333
monia <ornellagrace0412@gmail.com>
2025-07-11 16:45:11 +0200

    Adding new lines

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,3 +2,9 @@ process.argv.slice(2).forEach((arg, index) =>
   {
   console.log(`Argument ${index + 1}:${arg}`);
 });
+
+const [, , firstArg] = process.argv;
+if (firstArg) {
+  console.log(firstArg);
+}
+
